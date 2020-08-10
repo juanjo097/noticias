a8d1e7c24d87b132a854fb38927d34719d6d46d4
juanjo097 <rodriguez.rangel97@gmail.com>
2020-08-10 12:55:33 -0500

    agrega template admin funcionando, migraciones

---
+++ noticias/database/migrations/2020_08_10_174240_noticia.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Noticia extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Crear la entidad/tabla “Noticia”
+        Schema::create('noticia', function(BluePrint $table)
+        {
+            $table->id();
+            $table->string('title');
+            $table->string('summary');
+            $table->string('content');
+            $table->string('url_normalized');
+            $table->string('created_at');
+            $table->string('updated_at');
+        }
+        );
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ noticias/sql/create_squema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS `noticias`;
+CREATE DATABASE `noticias` 
+
+DEFAULT CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
+USE `noticias`;
+SET lc_time_names = 'es_ES';
